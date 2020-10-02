0bdc83aa90cf288fd21d058fb7d0ff2cbf50e6b3
ducls-0951 <le.sy.duc@sun-asterisk.com>
2020-10-02 13:36:09 +0700

    BookList

---
+++ booklist/app.js
@@ -0,0 +1,75 @@
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+function UI() {}
+
+UI.prototype.addBookToList = function(book) {
+  const list = document.getElementById('book-list');
+
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">x</a></td>
+  `;
+
+  list.appendChild(row);
+}
+
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+UI.prototype.showAlert = function(msg, className) {
+  const div = document.createElement('div');
+  div.className = `alert ${className}`;
+  div.appendChild(document.createTextNode(msg));
+  const container = document.querySelector('.container');
+  const form = document.querySelector('#book-form');
+  container.insertBefore(div, form);
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+}
+
+UI.prototype.deleteBook = function(target) {
+  if (target.className == 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title, author, isbn);
+  
+  const ui = new UI();
+
+  if (title === '' || author === '' || isbn === '') {
+    ui.showAlert('Please insert field', 'error');
+  } else {
+    ui.addBookToList(book);
+    ui.showAlert('Book added!', 'success');
+    ui.clearFields();
+  }
+
+  e.preventDefault();
+})
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  ui.showAlert('Book removed!', 'success');
+
+  e.preventDefault();
+})
